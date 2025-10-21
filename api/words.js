264dd9efe89d9e5d477b69d597b891d3f08f3576
Killian <91655353+Killian1803@users.noreply.github.com>
2025-10-21 11:48:22 +0200

    api dossier fichier

---
+++ api/words.js
@@ -0,0 +1,15 @@
+let mots = [];
+
+export default function handler(req, res){
+  if(req.method === 'POST'){
+    const { text, x, y, color } = req.body;
+    if(text && x!==undefined && y!==undefined && color){
+      mots.push({ text, x, y, color });
+    }
+    res.status(200).json({ success:true });
+  } else if(req.method === 'GET'){
+    res.status(200).json(mots);
+  } else {
+    res.status(405).end();
+  }
+}
